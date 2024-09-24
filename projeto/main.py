93af061d48a75d17e2d2e9deb51a977c6cf790da
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2024-09-24 18:25:34 -0300

    Delete projeto directory

--- projeto/main.py
+++
@@ -1,8 +0,0 @@
-import os
-from .modelos.pessoa import Pessoa
-from .modelos.profissao import Profissao
-
-pessoa1 = Pessoa("malu", 19, Profissao.PROFESSOR)
-
-os.system("clear||cls")
-print(pessoa1)
--- projeto/modelos/profissao.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Profissao(Enum):
-    DENTISTA = ("Dentista")
-    ENFERMEIRO = ("Enfermeiro")
-    PROFESSOR = ("Professor")
-
-    def __init__(self,nome: str) -> None:
-        self.nome = nome
